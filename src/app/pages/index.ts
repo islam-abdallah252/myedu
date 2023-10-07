ea0b86219134a3f886b15a33d1f9a60758cf94e3
Islam abdallah salem <54332986+islam-abdallah252@users.noreply.github.com>
2023-10-08 01:27:52 +0300

    subscription page part one

--- src/app/pages/index.ts
+++ src/app/pages/index.ts
@@ -1,2 +1,3 @@
 export * from "./home/home.page";
+export * from "./subscription/subscription.module";
 
---
+++ src/app/pages/subscription/subscription.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subscription',
+  templateUrl: 'subscription.page.html',
+  styleUrls: ['subscription.page.scss'],
+})
+export class SubscriptionPage {
+
+  constructor() { }
+
+}
